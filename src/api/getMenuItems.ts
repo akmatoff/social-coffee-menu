81d758878581729c8ae0eb8d758939782f017388
akmatoff <azim.akmatov1@gmail.com>
2025-06-23 03:35:45 +0600

    add language support, move menu to dynamic routes

--- src/api/getMenuItems.ts
+++ src/api/getMenuItems.ts
@@ -2,8 +2,16 @@ import type { MenuItem, MenuItemsParams, PaginatedResponse } from "../types";
 import { ENDPOINTS } from "../endpoints";
 import api from "./base";
 
-export default async function getMenuItems(params?: MenuItemsParams) {
+export default async function getMenuItems(
+  params?: MenuItemsParams,
+  lang?: string
+) {
   return api
-    .get<PaginatedResponse<MenuItem>>(ENDPOINTS.MENU_ITEMS, { params })
+    .get<PaginatedResponse<MenuItem>>(ENDPOINTS.MENU_ITEMS, {
+      params,
+      headers: {
+        "Accept-Language": lang || "ru",
+      },
+    })
     .then((res) => res.data.results);
 }
