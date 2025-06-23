7c26d39de33dd8b49209ee1145a7ccc50b005dff
akmatoff <azim.akmatov1@gmail.com>
2025-06-24 01:11:14 +0600

    fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 import node from "@astrojs/node";
 import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   output: "server",
   adapter: node({
     mode: "standalone",
